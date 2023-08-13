b2c65221a2416abcfbcf4b9cbdf44fe52a71add4
luis-moguea <luis.mogueadp@gmail.com>
2023-08-13 16:09:51 -0500

    profile picture update

--- src/components/animations/Photo.tsx
+++ src/components/animations/Photo.tsx
@@ -14,7 +14,8 @@ const Languages = ({ icon }: Props) => {
     >
       <Image
         width={isHigherThan480 ? "80px" : "100px"}
-        height={isHigherThan480 ? "unset" : "100px"}
+        height={isHigherThan480 ? "80" : "100px"}
+        objectFit="fill"
         borderRadius="50%"
         boxShadow="1px 1px 8px 1px rgba(0, 0, 0, 0.2)"
         src={icon}
